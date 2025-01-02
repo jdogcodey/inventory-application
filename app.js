bb6ac4aef3b7ab2126dbce58ce9859f5b0d54132
jdogcodey <jonnydawson@hotmail.com>
2025-01-02 16:43:40 +0000

    Utilise PORT from .env

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const indexRouter = require("./routes/index");
+const path = require("node:path");
+const PORT = process.env.PORT;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Inventory application running on Port: ${PORT}`);
+});

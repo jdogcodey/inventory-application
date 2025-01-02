89dc8fdc7b4c9032d3a9aa601ff2649c3d37b5f8
jdogcodey <jonnydawson@hotmail.com>
2025-01-02 21:37:58 +0000

    add renderHomepage function

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,1 +1,6 @@
 const db = require(`../db/queries`);
+
+async function renderHomepage(req, res) {
+  const allBooks = await db.getAllBooksWithGenreAuthor();
+  res.render(`../views/index`, { allBooks: allBooks });
+}

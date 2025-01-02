ec15b0f25c2ae76316382858250d8a1ce3b1b1df
jdogcodey <jonnydawson@hotmail.com>
2025-01-02 16:54:36 +0000

    Added a few db queries

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,23 @@
 const pool = require("./pool");
+
+async function getAllBooks() {
+  const { rows } = await pool.query("SELECT * FROM books;");
+  return rows;
+}
+
+async function getAllGenres() {
+  const { rows } = await pool.query("SELECT * FROM genres;");
+  return rows;
+}
+
+async function getAllAuthors() {
+  const { rows } = await pool.query("SELECT * FROM authors;");
+  return rows;
+}
+
+async function getBookById(id) {
+  const { rows } = await pool.query("SELECT * FROM  books WHERE id = ($1)", [
+    id,
+  ]);
+  return rows;
+}
